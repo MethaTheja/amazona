a7a9a68766faf0ea377a9a36ac8010f83eaa6d7a
MethaTheja <methathejasilie@gmail.com>
2022-10-07 16:25:11 +0530

    herrr

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -115,14 +115,6 @@ export default function CartScreen() {
                     >
                       Proceed to checkout
                     </Button>
-                    <Button
-                      type="button"
-                      onClick={checkoutHandler}
-                      variant="primary"
-                      disabled={cartItems.length === 0}
-                    >
-                      Proceed to checkout
-                    </Button>
                   </div>
                 </ListGroup.Item>
               </ListGroup>
